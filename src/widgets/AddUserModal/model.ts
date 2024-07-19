96d972f07b3535c40fe1c39d3d77901603f7d11f
Andrey ligiza <andrey.ligiza@mail.ru>
2024-07-19 12:43:08 +0300

    fix: исправление ошибки has no exported member

--- src/widgets/AddUserModal/model.ts
+++ src/widgets/AddUserModal/model.ts
@@ -1,5 +1,5 @@
 import { IBlockProps } from '@shared/lib/Block';
-import { AddUserForm } from 'widgets/AddChatForm';
+import { AddUserForm } from '@widgets/AddUserForm';
 
 export interface IAddUserModalProps extends IBlockProps {
     modalTitle: string,
