c53ffe0b90650c2b47eca3c56044cea0705df637
Andrey ligiza <andrey.ligiza@mail.ru>
2024-07-19 12:30:45 +0300

    fix: исправление ошибок линта в express.js

--- express.js
+++ express.js
@@ -9,11 +9,11 @@ const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
 
 app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
-  });
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
